3439febe9633f6fefc4b453b3016c34635c47ba6
Zoe A Miller <39541174+zoemiller@users.noreply.github.com>
2018-12-19 14:41:31 -0500

    loading an empty but one liner ontario code
    eventually shall be ontario code/s

---
+++ ontario/routeontario.py
@@ -0,0 +1,9 @@
+#/bin/python
+# from handler.databank import "?" ????
+import handler.databank as db
+#----------------------------------------------------------------
+
+#-----------------------------------------------------------------
+
+# def routeontario(VaultData, SimulationStartDate, SimulationEndDate, OntarioInfo):
+    
